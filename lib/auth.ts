2aab3fc839e835e8c254be92b82addc14727b509
Yash Gandhi <yashgandhi4505@gmail.com>
2025-03-29 22:28:52 +0530

    NextAuth

---
+++ lib/auth.ts
@@ -0,0 +1,64 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { dbConnect } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions : NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {label: "Email", type: "text"},
+                password: {label: "Password", type: "password"}
+            },
+            async authorize(credentials){
+                if(!credentials?.email || !credentials?.password){
+                    throw new Error("Invalid credentials");
+                }
+                try{
+                    await dbConnect();
+                    const user = await User.findOne({email: credentials.email})
+                    if(!user){
+                        throw new Error("User not found");
+                    }
+                    const isValid = await bcrypt.compare(
+                        credentials.password,
+                        user.password)
+                    if(!isValid){
+                        throw new Error("Invalid credentials");
+                    }
+                    return{
+                        id: user._id.toString(),
+                        email: user.email
+                    }
+                }catch {
+                    throw new Error("Database connection error");
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({token, user}){
+            if(user){
+                token.id = user.id
+            }
+            return token;
+        },
+        async session({session, token}){
+            if(session.user){
+                session.user.id = token.id as string
+            }
+            return session;
+        }
+    },
+    pages: {
+        signIn: "/login",
+        error: "/login"
+    },
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+    },
+    secret: process.env.NEXTAUTH_SECRET
+};
---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+    function middleware(){
+        return NextResponse.next();
+    },
+    {
+        callbacks: {
+            authorized: ({token, req})=> {
+                const {pathname} = req.nextUrl;
+                if(pathname.startsWith("/api/auth") || pathname === "/login" || pathname === "/register"){
+                    return true;
+                }
+                if(pathname === "/"|| pathname.startsWith("/api/videos")){
+                    return true;
+                }
+                return !!token;
+            }
+        }
+    }
+)
+export const config = {
+    matcher: ["/((?!_next/static|_next/image|favicon.ico|/public).*)"],
+};
