256f048f817227a8b08339d4203894ed543bea3d
Yash Gandhi <yashgandhi4505@gmail.com>
2025-03-30 17:10:04 +0530

    FileUpload

---
+++ app/api/imagekit-auth/route.ts
@@ -0,0 +1,19 @@
+import ImageKit from "imagekit"
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
+
+export async function GET() {
+    try{
+        return NextResponse.json(imagekit.getAuthenticationParameters());
+    }catch(error){
+        return NextResponse.json(
+            { error: "Imagekit Authentication Failed"},
+            {status: 500}
+        )
+    }
+}
---
+++ app/components/FileUpload.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState } from "react";
+import { IKUpload } from "imagekitio-next";
+import { Loader2 } from "lucide-react";
+import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
+
+interface FileUploadProps {
+    onSuccess: (res: IKUploadResponse) => void;
+    onProgess?: (progress: number) => void;
+    fileType?: "image" | "video";
+}
+
+export default function FileUpload({
+    onSuccess,
+    onProgess,
+    fileType = "image"
+}: FileUploadProps) {
+    const [uploading, setUploading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    
+  const onError = (err: {message: string}) => {
+    console.log("Error", err);
+    setError(err.message);
+    setUploading(false);
+  };
+  
+  const handleSuccess = (response: IKUploadResponse) => {
+    console.log("Success", response);
+    setUploading(false);
+    setError(null);
+    onSuccess(response);
+  };
+  
+  const handleProgress = (evt: ProgressEvent) => {
+    if(evt.lengthComputable && onProgess){
+        const percentComplete = (evt.loaded / evt.total) * 100;
+        onProgess(Math.round(percentComplete));
+    }
+  };
+  
+  const handleStartUpload = () => {
+    setUploading(true);
+    setError(null);
+  };
+
+  const validateFile = (file: File) => {
+    if(fileType === "video") {
+        if(!file.type.startsWith("video/")){
+            setError("Please upload a video file");
+            return false;
+        }
+        if(file.size > 100*1024*1024){
+            setError("File size exceeds 100MB");
+            return false;
+        }
+    }else {
+        const validTypes = ["image/jpeg", "image/png", "image/gif"];
+        if(!validTypes.includes(file.type)){
+            setError("Please upload a valid image file");
+            return false;
+        }
+        if(file.size > 5*1024*1024){
+            setError("File size exceeds 5MB");
+            return false;
+        }
+    }
+    return false;
+  }
+  return (
+    <div className="space-y-2">
+        <IKUpload
+          fileName={fileType === "video" ? "video" : "image"}
+          useUniqueFileName={true}
+          validateFile={validateFile}
+          onError={onError}
+          onSuccess={handleSuccess}
+          onUploadProgress={handleProgress}
+          onUploadStart={handleStartUpload}
+          folder={fileType === "video" ? "/videos" : "/images"}
+          className="file-input file-input-bordered w-full"
+          accept={fileType === 'video' ? 'video/*' : 'image/*'}
+        />
+        {
+            uploading && (
+                <div className="flex items-centern gap-2 text-sm text-primary">
+                    <Loader2 className="animate-spin w-4 h-4" />
+                    <span>Uploading...</span>
+                </div>
+            )
+        }
+        {error && <div className="text-red-500 text-sm">{error}</div>}
+    </div>
+  );
+}
